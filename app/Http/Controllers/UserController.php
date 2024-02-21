95de718d9c23936f38dd205b4ae3424208c00158
DivaAji <diva7aji7@gmail.com>
2024-02-21 19:36:47 +0700

    User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,9 +6,7 @@ class UserController extends Controller
 {
     public function profile($id, $name)
     {
-        // Di sini Anda dapat melakukan apapun yang perlu untuk menampilkan profil pengguna,
-        // misalnya mengambil data pengguna dari database berdasarkan ID.
-        $user = [
+      $user = [
             'id' => $id,
             'name' => $name
         ];
